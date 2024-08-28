e0860140aaa7ce0231a7e83352200e44493ff539
Alexa Lans <alexa@extralexa.nl>
2024-08-28 09:55:41 +0300

    Add data collector class to replace news and weather bards

---
+++ data_collector.py
@@ -0,0 +1,65 @@
+import requests
+
+class DataCollector:
+    def __init__(self, latitude, longitude, weather_api, news_api):
+        self.weather = {}
+        self.get_weather_data(latitude, longitude, weather_api)
+        self.news = ''
+        self.get_news_data(news_api)
+
+    def get_weather_data(self, latitude, longitude, weather_api):
+        weather_parameters = {
+            "lat": latitude,
+            "lon": longitude,
+            "units": "metric",
+            "cnt": 5,
+            "appid": weather_api,
+
+        }
+
+        weather_response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast",
+                                        params=weather_parameters)
+        weather_response.raise_for_status()
+
+        weather_data = weather_response.json()
+
+        rain_mm = 0
+        for item in weather_data["list"]:
+            try:
+                rain_mm += item['rain']['3h']
+            except KeyError:
+                pass
+
+        self.weather['temperature'] = round(weather_data["list"][1]["main"]["temp"])
+        self.weather['feel_temperature'] = round(weather_data["list"][1]["main"]["temp"])
+        self.weather['description'] = [item["weather"][0]["description"] + ', ' for item in weather_data["list"]]
+        self.weather['rain_mm'] = round(rain_mm)
+        print(self.weather)
+
+    def get_news_data(self, news_api):
+        news_parameters = {
+            "apikey": news_api,
+            "category": "science, technology, environment, tourism",
+            "language": "en",
+            "prioritydomain": "top",
+            "domainurl": "nytimes.com, bbc.com, aljazeera.com",
+            "size": 3,
+        }
+
+        news_response = requests.get(url="https://newsdata.io/api/1/latest", params=news_parameters)
+
+        news_response.raise_for_status()
+
+        news_data = news_response.json()['results']
+
+        for item in news_data:
+            self.news += f"{item['title']}: {item['description']}\n\n"
+
+
+#TODO GET TODO LIST FROM GOOGLE SHEETS OR TODOIST API
+
+
+#TODO USE BEAUTIFUL SOUP TO OBTAIN HOROSCOPE TEXT OR ASK CHATGPT TO COME UP WITH NONSENSE
+
+
+#TODO ADD GOOGLE CALENDAR EVENTS
--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-# ON HOLD | Suno does not have its own API and I cannot get the external APIs to work,
-# also cannot open a new automated browser because Google refuses to login user when
-# it knows you're botting. Possible solutions: open a different kind of browser,
-# try different login process (no authentication with Google), wait for Suno to release its own API.
-
-#For now.... just make it work on laptop with a trusted browser already opened and let pyautogui move around in it...
-
 import requests
 from dotenv import load_dotenv
 import os
 import datetime
-from weather_bard import WeatherBard
-from news_bard import NewsBard
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import time
 import pyautogui
 import PIL
+from data_collector import DataCollector
 from pyautogui import ImageNotFoundException
 
 #TODO move all preferences / settings to the top or different doc
@@ -35,96 +24,58 @@ NEWS_API = os.environ['NEWS_API']
 EMAIL_ADDRESS = os.environ['EMAIL_ADDRESS']
 EMAIL_PASSWORD = os.environ['EMAIL_PASSWORD']
 
-#START LYRICS
+data_collector = DataCollector(LATITUDE, LONGITUDE, WEATHER_API, NEWS_API)
 
-lyrics = (f"Hey there {NAME},\n"
-          f"a good morning to you,\n"
-          f"wake up, wake up,\n"
-          f"it's time to start the day\n\n")
+prompt = (f"Write a wake up song for someone named Alexa. \n"
+          f"Start with one verse about the weather today. It will rain {data_collector.weather['rain_mm']} millimeter.\n"
+          f"The temperature is going to be {data_collector.weather['temperature']} degrees and "
+          f"it feels like {data_collector.weather['feel_temperature']}.\n"
+          f"The weather can be described as {''.join(data_collector.weather['description'])}.\n\n"
+          f"The second verse includes a reminder about the person's to do list.\n"
+          f"Alexa wants to do yoga today, work on her coding skills and go for a run.\n"
+          f"Also she has to get motivated to play some guitar.\n\n"
+          f"For the bridge of the song, use information about the latest news. "
+          f"You can use this input for that: \n"
+          f"Please keep the news short and summarised."
+          f"{data_collector.news}")
 
-# GET LOCAL WEATHER FROM OPEN WEATHER
-
-# weather_parameters = {
-#     "lat": LATITUDE,
-#     "lon": LONGITUDE,
-#     "units": "metric",
-#     "cnt": 5,
-#     "appid": WEATHER_API,
-#
-# }
-#
-# weather_response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=weather_parameters)
-# weather_response.raise_for_status()
-#
-# weather_data = weather_response.json()
-# lyrics += WeatherBard(weather_data).lyrics
-
-#GET NEWS
-#
-# news_parameters = {
-#     "apikey": NEWS_API,
-#     "category": "science",
-#     #"country": "uk",
-#     "language": "en",
-#     "prioritydomain": "top",
-#     "domainurl": "nytimes.com, bbc.com, aljazeera.com",
-#     "size": 3,
-# }
-#
-# news_response = requests.get(url="https://newsdata.io/api/1/latest", params=news_parameters)
-#
-# news_response.raise_for_status()
-#
-# news_data = news_response.json()
-#
-# lyrics += NewsBard(news_data).lyrics
-
-#TODO GET TODO LIST FROM GOOGLE SHEETS OR TODOIST API
-
-
-#TODO USE BEAUTIFUL SOUP TO OBTAIN HOROSCOPE TEXT OR ASK CHATGPT TO COME UP WITH NONSENSE
-
-
-#TODO ADD GOOGLE CALENDAR EVENTS
-
+print(prompt)
 
 #TODO SAVE LYRICS IN FILE
 
 
-#TODO CREATE LYRICS
-
-#TODO CREATE SONG
 
 test_lyrics = "TEST LYRICS"
 test_genre = "ROCK ELECTRO SWING BOOGIE"
 test_title = "TEST TITLE"
 
-for i in range(5):
-    try:
-        pyautogui.scroll(200)
-        time.sleep(0.1)
-        pyautogui.click('pyautogui_images/enter_lyrics.png')
-        pyautogui.write(f"{test_lyrics}")
-        time.sleep(0.5)
-    except ImageNotFoundException:
-        pyautogui.keyDown('alt')
-        for _ in range(i):
-            pyautogui.press('tab')
-        pyautogui.keyUp('alt')
-        time.sleep(0.1)
-    else:
-        break
-
-pyautogui.click('pyautogui_images/enter_style.png')
-pyautogui.write(f"{test_genre}")
-
-pyautogui.scroll(-250)
-time.sleep(0.1)
-
-pyautogui.click('pyautogui_images/enter_title.png')
-pyautogui.write(f"{test_title}")
-
-pyautogui.click('pyautogui_images/create.png')
+# for i in range(5):
+#     try:
+#         pyautogui.scroll(200)
+#         time.sleep(0.1)
+#         pyautogui.click('pyautogui_images/enter_lyrics.png')
+#         pyautogui.write(f"{test_lyrics}")
+#         time.sleep(0.5)
+#     except ImageNotFoundException:
+#         pyautogui.keyDown('alt')
+#         for _ in range(i):
+#             pyautogui.press('tab')
+#         pyautogui.keyUp('alt')
+#         time.sleep(0.1)
+#     else:
+#         break
+#
+#
+# pyautogui.click('pyautogui_images/enter_style.png')
+# pyautogui.write(f"{test_genre}")
+#
+# pyautogui.scroll(-250)
+# time.sleep(0.1)
+#
+# pyautogui.click('pyautogui_images/enter_title.png')
+# pyautogui.write(f"{test_title}")
+#
+# pyautogui.click('pyautogui_images/create.png')
 
 
 
